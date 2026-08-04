c660c7bcf719451241e7aa9d43fb298c04d2bb0b
dapcastanheira-coder <dapcastanheira@gmail.com>
2026-08-04 12:38:29 +0200

    Update check_cardsnation.py

--- check_cardsnation.py
+++ check_cardsnation.py
@@ -18,6 +18,7 @@ URLS = [
     "https://www.alola.cz/elite-trainer-boxy/",
     "https://www.kitstore.cz/elite-trainer-box",
     "https://www.cdmc.cz/premiove-kolekce/pokemon-tcg--first-partner-illustration-collection-series-3/",
+    "https://www.zardo.cards/predobjednavky",
     "https://www.cdmc.cz/elite-trainer-boxy/",
     "https://www.smarty.sk/pokemon-tcg-30th-celebration-elite-trainer-box-4p278101",
     "https://www.cardstore.cz/pokemon-tcg--30th-celebration-elite-trainer-box/",

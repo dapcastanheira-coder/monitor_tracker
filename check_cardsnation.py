63a2c3ee313b3f3bd9f1a7d19ca2b14aad184885
dapcastanheira-coder <dapcastanheira@gmail.com>
2026-09-16 11:11:59 +0200

    Update check_cardsnation.py

--- check_cardsnation.py
+++ check_cardsnation.py
@@ -36,6 +36,8 @@ URLS = [
     "https://www.ihrysko.sk/pokemon-30th-celebration-booster-bundle-p122317",
 
     "https://www.alza.cz/EN/toys/pokemon-tcg-30th-celebration-elite-trainer-box-d13521013.htm",
+    "https://www.alza.cz/EN/toys/pokemon-tcg-30th-celebration-ex-tin-d13521014.htm",
+    
 ]
 
 
